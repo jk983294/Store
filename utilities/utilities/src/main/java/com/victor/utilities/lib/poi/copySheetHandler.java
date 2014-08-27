9a83a31de47673b023e89c28dc4662109d7a1823
jk983294 <jk983294@gmail.com>
2014-08-27 20:41:23 +0800

    add utilities

---
+++ utilities/utilities/src/main/java/com/victor/utilities/lib/poi/copySheetHandler.java
@@ -0,0 +1,38 @@
+package com.victor.utilities.lib.poi;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.xssf.model.SharedStringsTable;
+
+public class copySheetHandler extends ReadBaseHandler {
+	
+	private Sheet copysheet;
+	private Row sheetrow;
+	private Cell sheetcell;
+	
+	public copySheetHandler(SharedStringsTable sst) {
+		super(sst);
+		
+	}
+
+	@Override
+	public void cellValueHandle() {
+		if (isNewRow) {
+			sheetrow = copysheet.createRow((short) row -1);
+		}
+		sheetcell = sheetrow.createCell((short) column-1);
+		sheetcell.setCellValue(content);
+	}
+
+	public Sheet getCopysheet() {
+		return copysheet;
+	}
+
+	public void setCopysheet(Sheet copysheet) {
+		this.copysheet = copysheet;
+	}
+	
+	
+
+}
