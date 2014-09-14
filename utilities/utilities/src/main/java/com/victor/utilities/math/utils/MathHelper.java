daa48791939f7e38a5b3dc2a04ab4083b93aa7aa
jk983294 <jk983294@gmail.com>
2014-09-14 18:21:35 +0800

    utility add math helper for copy, swap and random variables

---
+++ utilities/utilities/src/main/java/com/victor/utilities/math/utils/MathHelper.java
@@ -0,0 +1,81 @@
+package com.victor.utilities.math.utils;
+
+import org.apache.commons.math3.util.MathArrays;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * Math Utils
+ */
+public class MathHelper {
+
+    private final static Random r = new Random();
+
+    /**
+     * generate uniform random variables in given bounds
+     * @param low
+     * @param up
+     * @return
+     */
+    public static double[] randomRange(double[] low, double[] up){
+        int len = up.length;
+        double[] rand = new double[len];
+        for (int i = 0; i < len; i++) {
+            rand[i] = randomRange(low[i], up[i]);
+        }
+        return rand;
+    }
+
+    public static double randomRange(double low, double up){
+        return (up - low) * r.nextDouble() + low;
+    }
+
+    public static List<double[]> randomRangeList(int size, double[] low, double[] up){
+        List<double[]> popList = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            popList.add(randomRange(low, up));
+        }
+        return popList;
+    }
+
+    /**
+     * swap elements in array
+     * @param parentIndex
+     * @param childIndex
+     * @param array
+     * @param <T>
+     */
+    public static <T> void swap(int parentIndex, int childIndex, T[] array) {
+        T parent = array[parentIndex];
+        array[parentIndex] = array[childIndex];
+        array[childIndex] = parent;
+    }
+
+    public static void swap(int parentIndex, int childIndex, double[] array) {
+        double parent = array[parentIndex];
+        array[parentIndex] = array[childIndex];
+        array[childIndex] = parent;
+    }
+
+    /**
+     * copy double array
+     * @param original
+     * @return
+     */
+    public static double[] copy(double[] original){
+        return MathArrays.copyOf(original);
+    }
+
+    public static double[] copy(double[] original, int len){
+        return MathArrays.copyOf(original, len);
+    }
+
+    public static double[] copy(double[] original, int from, int len){
+        return Arrays.copyOfRange(original, from, from + len);
+    }
+
+
+}
