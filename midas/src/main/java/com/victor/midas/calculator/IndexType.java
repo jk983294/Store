f15956fc9bb13b76590f6d18f5c5b07d0962fb83
jk983294 <jk983294@gmail.com>
2014-10-28 22:43:40 +0800

    delete midas

--- midas/src/main/java/com/victor/midas/calculator/IndexType.java
+++
@@ -1,8 +0,0 @@
-package com.victor.midas.calculator;
-
-/**
- * indicate which type this index value use
- */
-public enum IndexType {
-    DOUBLE, INT, BOOLEAN, UNKNOWN
-}
--- midas/src/main/java/com/victor/midas/services/worker/common/TaskStatus.java
+++
@@ -1,9 +0,0 @@
-package com.victor.midas.services.worker.common;
-
-public enum TaskStatus {
-	Submit,
-	Execute,
-	Pending,
-	Error,
-	Finished
-}
